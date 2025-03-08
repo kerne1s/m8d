0564a7a3b2cb6191fc22e0e0a90557f85c91976c
Dmitry Degtyarev <ddegtyarev@ronasit.com>
2025-03-08 10:52:25 +0600

    chore(private): update content

--- libs/web/secret-garden/features/view/src/lib/component.tsx
+++ libs/web/secret-garden/features/view/src/lib/component.tsx
@@ -6,22 +6,24 @@ import { secretGardenConfig } from './configuration';
 export function SecretGardenView(): ReactElement {
   const isEventStarted = new Date() > new Date(secretGardenConfig.eventDate);
   const videoSrc = isEventStarted ? 'https://cdn.pixabay.com/video/2020/07/22/45316-442643130_medium.mp4' : 'https://cdn.pixabay.com/video/2023/08/11/175540-853887878_small.mp4';
+  const posterSrc = isEventStarted ? 'images/theme-2.png' : 'images/theme-1.png';
   const quote = isEventStarted ? secretGardenConfig.quotes[1] : secretGardenConfig.quotes[0];
+  const playlist = isEventStarted ? secretGardenConfig.playlist.afterEvent : secretGardenConfig.playlist.beforeEvent;
 
   return (
     <div className='home min-h-screen no-scrollbar'>
       <div className='relative w-full h-screen'>
         <video
           src={videoSrc}
-          // poster='images/river.jpg'
+          poster={posterSrc}
           preload='none'
           autoPlay
           loop
           muted
           className='h-full w-full object-cover' />
         <Typewriter text={quote} className='fixed top-[120px] md:top-[50%] left-[10%] md:left-[100px] max-w-[500px]' />
-        <CountdownTimer className='w-[220px] fixed bottom-[140px] md:bottom-[100px] left-0 md:left-[unset] right-0 md:right-[100px] mx-auto md:mx-0' />
-        <AppAudioPlayer playList={secretGardenConfig.playlist} shouldAutoplay />
+        {!isEventStarted && <CountdownTimer className='w-[220px] fixed bottom-[140px] md:bottom-[100px] left-0 md:left-[unset] right-0 md:right-[100px] mx-auto md:mx-0' />}
+        <AppAudioPlayer playList={playlist} shouldAutoplay />
       </div>
     </div> 
   );
